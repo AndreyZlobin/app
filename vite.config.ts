ff9cc47595240a349239e30ca490fe1586ec61e4
Andrey <zlobin.andy@gmail.com>
2023-08-25 14:20:34 +0300

    re

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,56 @@
-import { defineConfig } from 'vite'
+import { defineConfig, Plugin } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from "@originjs/vite-plugin-federation";
 import {dependencies} from './package.json'
 
-console.log(dependencies)
+type FederationManifest = Record<string, {
+    name: string;
+    filename: string;
+    exposes: Record<string, string>
+}>
+
+const manifest: FederationManifest = {
+    app: {
+        name: 'remote-app',
+        filename: 'remoteEntry.js',
+        exposes: {
+            './AuthModal': './src/remote/auth-modal/index.ts',
+            './Button': './src/components/Button.tsx',
+            './Header': './src/components/Header.tsx',
+            './Footer': './src/components/Footer.tsx',
+        }
+    }
+}
+
+// const Tttt = () => {
+//     manifest.
+// }
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
       federation({
-      name: 'remote-app',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './AuthModal': './src/remote/auth-modal/index.ts',
-        './Button': './src/components/Button.tsx',
-        './Header': './src/components/Header.tsx',
-        './Footer': './src/components/Footer.tsx',
-      },
+          name: 'remote-app',
+          filename: 'remoteEntry.js',
+          exposes: {
+            './AuthModal': './src/remote/auth-modal/index.ts',
+            './Button': './src/components/Button.tsx',
+            './Header': './src/components/Header.tsx',
+            './Footer': './src/components/Footer.tsx',
+          },
       shared: dependencies
     }),
+      federation({
+          name: 'remote-app2',
+          filename: 'remoteEntry2.js',
+          exposes: {
+              './AuthModal': './src/remote/auth-modal/index.ts',
+              './Button': './src/components/Button.tsx',
+              './Header': './src/components/Header.tsx',
+              './Footer': './src/components/Footer.tsx',
+          },
+          shared: dependencies
+      }),
   ],
   build: {
     modulePreload: false,
