fafe8b6828db27fdf93aadbe28e0905707fe2f57
Andrey <zlobin.andy@gmail.com>
2023-08-25 16:19:50 +0300

    update

---
+++ lib/create-federation.ts
@@ -0,0 +1,22 @@
+import federation, {VitePluginFederationOptions} from "@originjs/vite-plugin-federation";
+import {Plugin} from "vite";
+import {writeFileSync} from 'fs';
+import {resolve} from 'path';
+
+const createManifest = (content: string) => {
+    writeFileSync(resolve(process.cwd(), 'federation-manifest.json'), content);
+}
+
+export const CreateFederation = (options: VitePluginFederationOptions): Plugin => {
+    const originFederationPlugin =  federation(options)
+    const manifest = {
+        [options.name]: `{remote}/${options.filename}`
+    }
+    return {
+        name: 'federation-plugin',
+        ...originFederationPlugin,
+        buildEnd(){
+            createManifest(JSON.stringify(manifest, null, 2))
+        }
+    };
+}
--- vite.config.ts
+++ vite.config.ts
@@ -2,45 +2,29 @@ import { defineConfig, Plugin } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation, {VitePluginFederationOptions} from "@originjs/vite-plugin-federation";
 import {dependencies} from './package.json'
-//
+import {CreateFederation} from "./lib/create-federation";
        const  exposes =  {
             './AuthModal': './src/remote/auth-modal/index.ts',
             './Button': './src/components/Button.tsx',
             './Header': './src/components/Header.tsx',
             './Footer': './src/components/Footer.tsx',
         }
-
-        const CreateFederation = (options: VitePluginFederationOptions): Plugin => {
-            const fed =  federation(options)
-            console.log(fed)
-            const manifest = {
-                [options.name]: `{remote}/${options.filename}`
-            }
-            console.log(manifest)
-            return {
-                name: 'auto-exposes-plugin',
-                ...fed,
-                buildEnd(){
-                    const manifest = {
-                    [options.name]: `{remote}/${options.filename}`
-                }
-                    console.log(manifest)
-                }
-            };
-        }
-export default defineConfig({
-  plugins: [react(),
-      CreateFederation({
-          name: 'AuthModal',
-          filename: 'remoteEntry.js',
-          exposes,
-          shared: dependencies
-      })
-  ],
-  build: {
-    modulePreload: false,
-    target: "esnext",
-    minify: false,
-    cssCodeSplit: false,
-  },
+export default defineConfig(() => {
+    console.log(process.env)
+    return {
+        plugins: [react(),
+            CreateFederation({
+                name: 'AuthModal',
+                filename: 'remoteEntry.js',
+                exposes,
+                shared: dependencies
+            })
+        ],
+        build: {
+            modulePreload: false,
+            target: "esnext",
+            minify: false,
+            cssCodeSplit: false,
+        },
+    }
 })
