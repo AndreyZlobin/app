738e76cb4240935f9d81b82cc47fcabef7a14550
Andrey <zlobin.andy@gmail.com>
2023-08-25 14:34:54 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,26 @@
-import { defineConfig, Plugin } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from "@originjs/vite-plugin-federation";
 import {dependencies} from './package.json'
+//
+// type FederationManifest = Record<string, {
+//     name: string;
+//     filename: string;
+//     exposes: Record<string, string>
+// }>
 
-type FederationManifest = Record<string, {
-    name: string;
-    filename: string;
-    exposes: Record<string, string>
-}>
-
-const manifest: FederationManifest = {
-    app: {
-        name: 'remote-app',
-        filename: 'remoteEntry.js',
-        exposes: {
-            './AuthModal': './src/remote/auth-modal/index.ts',
-            './Button': './src/components/Button.tsx',
-            './Header': './src/components/Header.tsx',
-            './Footer': './src/components/Footer.tsx',
-        }
-    }
-}
-
-// const Tttt = () => {
-//     manifest.
+// const manifest: FederationManifest = {
+//     app: {
+//         name: 'remote-app',
+//         filename: 'remoteEntry.js',
+//         exposes: {
+//             './AuthModal': './src/remote/auth-modal/index.ts',
+//             './Button': './src/components/Button.tsx',
+//             './Header': './src/components/Header.tsx',
+//             './Footer': './src/components/Footer.tsx',
+//         }
+//     }
 // }
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
       federation({
