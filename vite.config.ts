c842b3708199e496fb5365b21e88670985b64e3e
Andrey <zlobin.andy@gmail.com>
2023-09-12 08:45:56 +0300

    feat: Uu

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, Plugin } from 'vite'
 import react from '@vitejs/plugin-react'
-import federation, {VitePluginFederationOptions} from "@originjs/vite-plugin-federation";
+// import federation, {VitePluginFederationOptions} from "@originjs/vite-plugin-federation";
 import {dependencies} from './package.json'
 import {CreateFederation} from "./lib/create-federation";
        const  exposes =  {
@@ -9,16 +9,31 @@ import {CreateFederation} from "./lib/create-federation";
             './Header': './src/components/Header.tsx',
             './Footer': './src/components/Footer.tsx',
         }
-export default defineConfig(() => {
-    console.log(process.env)
+
+import { federation } from '@module-federation/vite';
+import { createEsBuildAdapter } from '@softarc/native-federation-esbuild';
+import { reactReplacements } from '@softarc/native-federation-esbuild/src/lib/react-replacements';
+export default defineConfig(async ({command}) => {
     return {
         plugins: [react(),
-            CreateFederation({
-                name: 'AuthModal',
-                filename: 'remoteEntry.js',
-                exposes,
-                shared: dependencies
-            })
+
+            await federation({
+                options: {
+                    workspaceRoot: __dirname,
+                    outputPath: 'dist',
+                    tsConfig: 'tsconfig.json',
+                    federationConfig: `module-federation/federation.config.cjs`,
+                    verbose: false,
+                    dev: command === 'serve',
+                },
+                adapter: createEsBuildAdapter({ plugins: [], fileReplacements: reactReplacements.dev }),
+            }),
+            // CreateFederation({
+            //     name: 'AuthModal',
+            //     filename: 'remoteEntry.js',
+            //     exposes,
+            //     shared: dependencies
+            // })
         ],
         build: {
             modulePreload: false,
