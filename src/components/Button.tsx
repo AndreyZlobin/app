898b0315255a3fe65b6a2a77a149b05bfabeb6e4
Andrey <zlobin.andy@gmail.com>
2023-09-12 09:23:29 +0300

    feat: Uu

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,15 @@
 import {useState} from "react";
 
+console.log(useState)
 const Button = () => {
-    const [count, setCount] = useState(1)
-    const handleCount = () => {
-        setCount(prev => prev+=1)
-    }
+    // const [count, setCount] = useState(1)
+    // const handleCount = () => {
+    //     setCount(prev => prev+=1)
+    // }
     return <>
     external app from vercel
-        <button onClick={handleCount}>count: {count}</button>
-
+        {/*<button onClick={handleCount}>count: {count}</button>*/}
+<button>sdsdsdsd</button>
     </>
 }
 
