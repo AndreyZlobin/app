3fa0a6f80f9c2f0aa537a481c9a6d5a093511903
Andrey <zlobin.andy@gmail.com>
2023-09-13 12:14:54 +0300

    feat

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,10 @@
 import {memo, useCallback, useState} from "react";
 
-console.log(useState)
 const Button = memo(() => {
     const [count, setCount] = useState(1)
     const handleCount = useCallback(() => {
         setCount(prev => prev+=1)
     }, [])
-    console.log(count)
     return <div>
     external app from vercel
         <button onClick={handleCount}>count: {count}</button>
--- vite.config.ts
+++ vite.config.ts
@@ -4,32 +4,30 @@ import {dependencies} from './package.json'
 import {CreateFederation} from "./lib/create-federation";
 import topLevelAwait from 'vite-plugin-top-level-await'
        const  exposes =  {
-            './AuthModal': './src/remote/auth-modal/index.ts',
+            // './AuthModal': './src/remote/auth-modal/index.ts',
             './Button': './src/components/Button.tsx',
-            './Header': './src/components/Header.tsx',
-            './Footer': './src/components/Footer.tsx',
-           './Context': 'src/context.tsx',
-           './sum': 'src/sum.ts'
+            // './Header': './src/components/Header.tsx',
+            // './Footer': './src/components/Footer.tsx',
+           // './Context': 'src/context.tsx',
+           // './sum': 'src/sum.ts'
         }
 
-export default defineConfig(async ({command}) => {
-    console.log(command)
+export default defineConfig(async ({}) => {
     return {
-
         plugins: [
             react(),
             CreateFederation({
-                name: 'AuthModal',
+                name: 'dynamic-remote',
                 filename: 'remoteEntry.js',
                 exposes,
-                shared: {
-                    react:{
-                        generate:false
-                    },
-                    'react-dom':{
-                        generate:false
-                    },
-    }
+                // shared: {
+                //     react: {
+                //         generate: false,
+                //     },
+                //     'react-dom': {
+                //         generate: false
+                //     },
+                // }
                 // shared: {
                 // "@emotion/react": "11.11.1",
                 // "@emotion/styled": "11.11.0",
@@ -40,10 +38,12 @@ export default defineConfig(async ({command}) => {
             }),
             topLevelAwait({
                 // The export name of top-level await promise for each chunk module
-                promiseExportName: "__tla",
+                promiseExportName: '__tla',
                 // The function to generate import names of top-level await promise in each chunk module
-                promiseImportName: i => `__tla_${i}`
-            })
+                promiseImportName: (i) => {
+                    return `__tla_${i}`;
+                },
+            }),
         ],
         build: {
             assetsInlineLimit: 40960,
