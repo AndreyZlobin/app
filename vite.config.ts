740cffa6dbfc102bed03d71c070314e799600043
Andrey <zlobin.andy@gmail.com>
2023-09-13 11:52:28 +0300

    feat: Update

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,14 @@ export default defineConfig(async ({command}) => {
                 name: 'AuthModal',
                 filename: 'remoteEntry.js',
                 exposes,
-                shared: ['react', 'react-dom']
+                shared: {
+                    react:{
+                        generate:false
+                    },
+                    'react-dom':{
+                        generate:false
+                    },
+    }
                 // shared: {
                 // "@emotion/react": "11.11.1",
                 // "@emotion/styled": "11.11.0",
